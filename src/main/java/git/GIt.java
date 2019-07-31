1bef8241191d43f820f175f05bcad3087dcb5838
Tarry <10086@163.com>
2019-07-31 20:29:00 +0800

    第2次提交

--- src/main/java/git/GIt.java
+++ src/main/java/git/GIt.java
@@ -2,8 +2,6 @@ package git;
 
 public class GIt {
     public static void main(String[] args) {
-        System.out.println("hellow,git");
-        System.out.println("hellow,git");
-        System.out.println("hellow,git");
+        System.out.println(66);
     }
 }
